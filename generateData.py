a339adf9d9f86daa63cf7d9d45cf0c5d9fd1e57c
ShvetankPrakash <s.prakash@columbia.edu>
2021-05-05 08:49:31 -0700

    Cleaning up code.

--- generateData.py
+++ generateData.py
@@ -1,14 +1,13 @@
-#!/bin/python3.8
-
 import sys
+import numpy as np
 
 def chopFile(filename):
    f = open(filename, "r")
-   print(f.read())
+   print(f.read(80*80))
 
 if __name__ == "__main__":
    # filename to be chopped passed as command line arg
    if len(sys.argv) != 2:
-      print("Usage: ./generateData.py <filename>")
+      print("Usage: python generateData.py <filename.py>")
    else:  
       chopFile(sys.argv[1])

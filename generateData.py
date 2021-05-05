a2ffb1f7f9cac84848f5027c669e1f9160ab4a6f
ShvetankPrakash <s.prakash@columbia.edu>
2021-05-05 10:45:37 -0700

    Completed code for chopping src file into 80x80 char arrays.

--- generateData.py
+++ generateData.py
@@ -1,9 +1,51 @@
 import sys
 import numpy as np
 
+
+# Dim of square code blocks
+SIZE = 80
+
+
 def chopFile(filename):
+   # Open file and create SIZExSIZE char arr to chop code into blocks
    f = open(filename, "r")
-   print(f.read(80*80))
+   matrix = np.zeros((SIZE, SIZE), dtype=np.uint8)
+   codeBlocks = []
+
+   char = f.read(1)
+   row = 0
+   col = 0
+   while char != '':
+      asciiVal = ord(char)
+      matrix[row, col] = asciiVal
+
+      # Increment & read next char
+      col += 1
+      if char == '\n' or col == SIZE:
+         row += 1
+         col = 0
+      col = col % SIZE
+      char = f.read(1)
+
+      # End of code block reached, create new block
+      if row == SIZE:
+         codeBlocks.append(matrix) 
+         matrix = np.zeros((SIZE, SIZE), dtype=np.uint8)
+         row = 0
+         col = 0
+
+   if not (matrix == 0).all():
+      codeBlocks.append(matrix) 
+      
+
+   # Write chopped blocks to .npy file
+   # TODO
+   # print(codeBlocks)
+
+   f.close()
+   return codeBlocks
+
+       
 
 if __name__ == "__main__":
    # filename to be chopped passed as command line arg

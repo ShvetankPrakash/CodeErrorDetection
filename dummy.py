a89d020a06b598a87f3e0320b32ac2a5b583a984
ShvetankPrakash <s.prakash@columbia.edu>
2021-05-05 10:47:23 -0700

    Created fummy python file for testing.

---
+++ dummy.py
@@ -0,0 +1,32 @@
+# DUMMY TEST FILE
+import sys
+import numpy as np
+
+def foo():
+   print("Hello")
+
+def bar():
+   print("World!")
+
+if __name__ == "__main__":
+   foo()
+   bar()
+
+# DUMMY LINE
+# DUMMY LINE
+# DUMMY LINE
+# DUMMY LINE
+# DUMMY LINE
+# DUMMY LINE
+# DUMMY LINE
+# DUMMY LINE
+# DUMMY LINE
+# DUMMY LINE
+# DUMMY LINE
+# DUMMY LINE
+# DUMMY LINE
+# DUMMY LINE
+# DUMMY LINE
+# DUMMY LINE
+
+# NEXT DUMMY LINE

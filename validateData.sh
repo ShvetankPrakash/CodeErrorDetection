b5498a9b87da8191bc098a6a8085bb108e4404ff
ShvetankPrakash <s.prakash@columbia.edu>
2021-05-06 11:06:26 -0700

    More robust and improved script for checking files.

--- validateData.sh
+++ validateData.sh
@@ -1,11 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
-# Skeleton script to validate the data (code blocks)
+# Script to validate the data (code blocks)
 # are valid and correct
 
 codeDir="./pythonCode"
 
+validateFileBlocks() { 
+   for file in "$@"
+   do
+      python testChopping.py $file > /dev/null 2>&1
+      if [ $? -ne 0 ]; then
+         echo FAILED: $file 
+      fi
+   done
+}
+export -f validateFileBlocks
+
 for dir in "$codeDir"/*
 do
-   find $dir -name "*.py" -exec python testChopping.py {} \;
+   find $dir -name "*.py" -exec bash -c 'validateFileBlocks "$@"' bash {} +;
+   #find $dir -name "*.py" -exec python testChopping.py {} \;
 done

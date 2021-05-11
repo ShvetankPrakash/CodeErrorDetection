e107bd8f0ec88f505c3dd7d3c559cff1db23933a
ShvetankPrakash <s.prakash@columbia.edu>
2021-05-11 15:01:01 -0700

    Filled in most of body of skeleton script for generating dataset.

--- readDataset.py
+++ readDataset.py
@@ -1,16 +1,34 @@
+# Script to create dataset for TF from numpy files
+
 import numpy as np
-import glob, os
+import glob, os, sys
 
 def readDataset(directory):
+   # Create dataset with labels for training
+   dataset = np.zeros((0, 80, 80), dtype=np.uint8)
+   labels  = np.zeros((0), dtype = np.uint8)
+
    os.chdir(directory)
    for filename in glob.glob("*.npy"):
-       print(filename)
-   
+      codeBlock = np.load(filename, allow_pickle=True) 
+      
+      # Add error-free code block to dataset
+      dataset = np.insert(dataset, dataset.shape[0], codeBlock, 0)
+      labels = np.append(labels, 1)
 
- 
+      # Add error to code block and add to dataset
+      errorBlock = codeBlock 
+      dataset = np.insert(dataset, dataset.shape[0], errorBlock, 0)
+      labels = np.append(labels, 0)
+  
+   print (dataset.shape)
+   print(labels.shape) 
+   return dataset, labels
+      
+
 if __name__ == "__main__":
-   # filename to be chopped passed as command line arg
+   # directory containing .npy files for dataset passed as command line arg
    if len(sys.argv) != 2:
       print("Usage: python readDataset.py <dataset_dir>")
    else:  
-      chopFile(sys.argv[1])
+      readDataset(sys.argv[1])

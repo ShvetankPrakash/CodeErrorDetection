daeb13cd0c18120f85cba384a1f613832a994339
ShvetankPrakash <s.prakash@columbia.edu>
2021-05-11 17:58:00 -0700

    Initial exploration to get feel of some baseline results.

--- train.py
+++ train.py
@@ -13,19 +13,19 @@ from readDataset import readDataset
 DATASET_DIR = "./dataset"
 BATCH_SIZE = 64
 SHUFFLE_BUFFER_SIZE = 100
-   
+EPOCHS = 100
 
 def getDataset():
    # Read Dataset
    dataset, labels = readDataset(DATASET_DIR)
 
    # Split Dataset (70/30 split)
-   seventy       = np.floor(dataset.shape[0] * 0.7)
-   trainExamples = dataset[:seventy]
-   trainLabels   = labels[:seventy]   
+   seventy       = int(np.floor(dataset.shape[0] * 0.7))
+   trainExamples = dataset[: seventy]
+   trainLabels   = labels[: seventy]   
 
-   testExamples = dataset[seventy:]
-   testLabels   = labels[seventy:]
+   testExamples = dataset[seventy :]
+   testLabels   = labels[seventy :]
 
    # Create TF Dataset objects
    trainDataset = tf.data.Dataset.from_tensor_slices((trainExamples, trainLabels))
@@ -39,24 +39,25 @@ def train():
    trainDataset, testDataset = getDataset() 
 
    # Shuffle and batch the datasets
-   train_dataset = train_dataset.shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)
-   test_dataset = test_dataset.batch(BATCH_SIZE)
+   trainDataset = trainDataset.shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)
+   testDataset = testDataset.batch(BATCH_SIZE)
 
    # Build and train model
    model = tf.keras.Sequential([
-       tf.keras.layers.Flatten(input_shape=(28, 28)),
+       tf.keras.layers.Flatten(input_shape=(80, 80)),
        tf.keras.layers.Dense(128, activation='relu'),
-       tf.keras.layers.Dense(10)
+       tf.keras.layers.Dense(2)
    ])
    
    model.compile(optimizer=tf.keras.optimizers.RMSprop(),
                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
                  metrics=['sparse_categorical_accuracy'])
    
-   model.fit(train_dataset, epochs=10)
+   model.fit(trainDataset, epochs=EPOCHS)
 
    # Evaluate model 
-   model.evaluate(test_dataset)
+   print("-------------------EVALUATE ON TEST SET--------------------")
+   model.evaluate(testDataset)
 
 
 if __name__ == "__main__":

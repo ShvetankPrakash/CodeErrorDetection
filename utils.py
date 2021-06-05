ede21058a20ab3517706ba74d33c73eb20ff9414
ShvetankPrakash <s.prakash@columbia.edu>
2021-06-05 16:31:35 -0700

    Added another helper function for determining if a string is valid Python code.

--- utils.py
+++ utils.py
@@ -30,3 +30,11 @@ def codeBlockToString(codeBlock):
    return codeString 
 
 
+def is_valid_python(code):
+   try:
+       ast.parse(code)
+   except SyntaxError:
+       return False
+   return True
+
+
